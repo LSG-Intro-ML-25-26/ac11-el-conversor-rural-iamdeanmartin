2a180e5aff93e8af2699cd45f32e1977f2fbc7ef
Dean Martin García <dean.martin@gracia.lasalle.cat>
2025-12-14 22:29:02 +0100

    Creamos la funcion para que el juego detecto si estamos haciendo overlaping al comerciante y creamos la variable boleana para disparar las acciones.

--- main.py
+++ main.py
@@ -23,12 +23,9 @@ def on_left_pressed():
         False)
 controller.left.on_event(ControllerButtonEvent.PRESSED, on_left_pressed)
 
-#Sprite del personaje jugador, posicionamiento y movimiento.
 nena: Sprite = None
-chest2: Sprite = None
-comerciante2: Sprite = None
-portal2: Sprite = None
-
+# Variable para comerciar
+cerca_comerciante2 = False
 tiles.set_current_tilemap(tilemap("""
     nivel_juego
     """))
@@ -37,38 +34,15 @@ nena = sprites.create(assets.image("""
     """), SpriteKind.player)
 tiles.place_on_tile(nena, tiles.get_tile_location(2, 4))
 controller.move_sprite(nena, 100, 0)
-# Sprite del cofre y posicionamiento.
-chest2 = sprites.create(assets.image("""
-    cofre
-    """), SpriteKind.chest)
-tiles.place_on_tile(chest2, tiles.get_tile_location(0, 4))
 # Sprite del comerciante y posicionamiento.
 comerciante2 = sprites.create(assets.image("""
         comerciante
         """),
     SpriteKind.comerciante)
 tiles.place_on_tile(comerciante2, tiles.get_tile_location(4, 4))
-# Sprite del portal y posicionamiento.
-portal2 = sprites.create(assets.image("""
-    portal
-    """), SpriteKind.portal)
-tiles.place_on_tile(portal2, tiles.get_tile_location(7, 4))
 
-#Valores de intercambio de objetos.
-diccionario_intercambio = {
-    "gallina": 6.0,
-    "patata": 2.0/1.5,
-    "cabra": 5.0,
-    "huevo": 3.0/12.0,
-    "caballo": 12.0
-}
+def on_update():
+    global cerca_comerciante2
+    cerca_comerciante2 = nena.overlaps_with(comerciante2)
 
-#inventario inicial del jugador.
-inventario_inicial_nena = {
-    "madera": 25.0,
-    "gallina": 2,
-    "patata": 5.5,
-    "cabra": 0,
-    "huevo": 3,
-    "caballo": 0
-}
+game.on_update(on_update)
--- main.ts
+++ main.ts
@@ -4,61 +4,31 @@ namespace SpriteKind {
     export const comerciante = SpriteKind.create()
     export const portal = SpriteKind.create()
 }
-
-controller.right.onEvent(ControllerButtonEvent.Pressed, function on_right_pressed() {
-    animation.runImageAnimation(nena, assets.animation`
-            nena-animation-right
-            `, 500, false)
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    nena,
+    assets.animation`nena-animation-right`,
+    500,
+    false
+    )
 })
-controller.left.onEvent(ControllerButtonEvent.Pressed, function on_left_pressed() {
-    animation.runImageAnimation(nena, assets.animation`
-            nena-animation-left
-            `, 500, false)
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    nena,
+    assets.animation`nena-animation-left`,
+    500,
+    false
+    )
 })
-// Sprite del personaje jugador, posicionamiento y movimiento.
-let nena : Sprite = null
-let chest2 : Sprite = null
-let comerciante2 : Sprite = null
-let portal2 : Sprite = null
-tiles.setCurrentTilemap(tilemap`
-    nivel_juego
-    `)
-nena = sprites.create(assets.image`
-    nena-front
-    `, SpriteKind.Player)
+let cerca_comerciante2 = false
+let nena: Sprite = null
+tiles.setCurrentTilemap(tilemap`nivel_juego`)
+nena = sprites.create(assets.image`nena-front`, SpriteKind.Player)
 tiles.placeOnTile(nena, tiles.getTileLocation(2, 4))
 controller.moveSprite(nena, 100, 0)
-//  Sprite del cofre y posicionamiento.
-chest2 = sprites.create(assets.image`
-    cofre
-    `, SpriteKind.chest)
-tiles.placeOnTile(chest2, tiles.getTileLocation(0, 4))
-//  Sprite del comerciante y posicionamiento.
-comerciante2 = sprites.create(assets.image`
-        comerciante
-        `, SpriteKind.comerciante)
+// Sprite del comerciante y posicionamiento.
+let comerciante2 = sprites.create(assets.image`comerciante`, SpriteKind.comerciante)
 tiles.placeOnTile(comerciante2, tiles.getTileLocation(4, 4))
-//  Sprite del portal y posicionamiento.
-portal2 = sprites.create(assets.image`
-    portal
-    `, SpriteKind.portal)
-tiles.placeOnTile(portal2, tiles.getTileLocation(7, 4))
-// Valores de intercambio de objetos.
-let diccionario_intercambio = {
-    "gallina" : 6.0,
-    "patata" : 2.0 / 1.5,
-    "cabra" : 5.0,
-    "huevo" : 3.0 / 12.0,
-    "caballo" : 12.0,
-}
-
-// inventario inicial del jugador.
-let inventario_inicial_nena = {
-    "madera" : 25.0,
-    "gallina" : 2,
-    "patata" : 5.5,
-    "cabra" : 0,
-    "huevo" : 3,
-    "caballo" : 0,
-}
-
+game.onUpdate(function () {
+    cerca_comerciante2 = nena.overlapsWith(comerciante2)
+})
