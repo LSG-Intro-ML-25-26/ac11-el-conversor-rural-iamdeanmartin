a48fb596cfce6d4c92c6337eecd65bcc336f443e
Dean Martin García <dean.martin@gracia.lasalle.cat>
2025-12-16 15:28:58 +0100

    Introducimos de nuevo todos los sprites, los colocamos en el mapa y ponemos el teselas para limitar el movimiento del jugador.

--- main.py
+++ main.py
@@ -4,22 +4,45 @@ class SpriteKind:
     chest = SpriteKind.create()
     comerciante = SpriteKind.create()
     portal = SpriteKind.create()
-calculadora_intercambio = sprites.create(img("""
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
+    material = SpriteKind.create()
+
+def on_right_pressed():
+    animation.run_image_animation(nena,
+        assets.animation("""
+            nena-animation-right
+            """),
+        500,
+        False)
+controller.right.on_event(ControllerButtonEvent.PRESSED, on_right_pressed)
+
+def on_left_pressed():
+    animation.run_image_animation(nena,
+        assets.animation("""
+            nena-animation-left
+            """),
+        500,
+        False)
+controller.left.on_event(ControllerButtonEvent.PRESSED, on_left_pressed)
+
+nena: Sprite = None
+scene.set_background_image(assets.image("""
+    background
+    """))
+tiles.set_current_tilemap(tilemap("""
+    nivel7
+    """))
+nena = sprites.create(assets.image("""
+    nena-front
+    """), SpriteKind.player)
+controller.move_sprite(nena, 100, 0)
+nena.set_stay_in_screen(True)
+tiles.place_on_tile(nena, tiles.get_tile_location(4, 5))
+trader = sprites.create(assets.image("""
+        comerciante
         """),
-    SpriteKind.player)
+    SpriteKind.comerciante)
+tiles.place_on_tile(trader, tiles.get_tile_location(1, 5))
+arbol = sprites.create(assets.image("""
+    árbol
+    """), SpriteKind.material)
+tiles.place_on_tile(arbol, tiles.get_tile_location(7, 4))
--- main.ts
+++ main.ts
@@ -3,6 +3,50 @@ namespace SpriteKind {
     export const chest = SpriteKind.create()
     export const comerciante = SpriteKind.create()
     export const portal = SpriteKind.create()
+    export const material = SpriteKind.create()
 }
-tiles.setCurrentTilemap(tilemap`nivel_juego`)
-let calculadora_intercambio = sprites.create(assets.image`miImagen`, SpriteKind.Player)
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    nena,
+    assets.animation`nena-animation-down`,
+    500,
+    false
+    )
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    nena,
+    assets.animation`nena-animation-right`,
+    500,
+    false
+    )
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    nena,
+    assets.animation`nena-animation-left`,
+    500,
+    false
+    )
+})
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    nena,
+    assets.animation`nena-animation-up`,
+    500,
+    false
+    )
+})
+let nena: Sprite = null
+scene.setBackgroundImage(assets.image`background`)
+tiles.setCurrentTilemap(tilemap`nivel7`)
+nena = sprites.create(assets.image`nena-front`, SpriteKind.Player)
+controller.moveSprite(nena, 100, 100)
+nena.setStayInScreen(true)
+tiles.placeOnTile(nena, tiles.getTileLocation(4, 5))
+let trader = sprites.create(assets.image`comerciante`, SpriteKind.comerciante)
+tiles.placeOnTile(trader, tiles.getTileLocation(1, 5))
+let arbol = sprites.create(assets.image`árbol`, SpriteKind.material)
+tiles.placeOnTile(arbol, tiles.getTileLocation(6, 5))
+let arbol2 = sprites.create(assets.image`árbol`, SpriteKind.material)
+tiles.placeOnTile(arbol2, tiles.getTileLocation(8, 6))
